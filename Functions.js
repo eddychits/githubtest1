cb711860b852d65951a9cadd9f0d8408526c78c9
eddychits <chits90000@gmail.com>
2025-03-07 12:53:32 +0300

    Create Functions.js
    Creating a function that has if else statement and printing the function

---
+++ Functions.js
@@ -0,0 +1,12 @@
+let str = (num1,num2) =>
+    {
+        if(num1<0 ||num2<0)
+         {
+            console.log('one of the numbers is negative');
+            
+        }
+        else
+        console.log(num1+num2);
+        }
+    str (0,-7)
+    
